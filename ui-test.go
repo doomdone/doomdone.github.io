1cbc2aef9ea6282b9250b9601d2d6111b2566571
damino <soopahero@MacBook-Havoq.local>
2020-04-05 17:41:24 +0300

    added tag

--- ui-test.go
+++ ui-test.go
@@ -1,1 +1,17 @@
 package ui
+
+import (
+	"net/http"
+	"testing"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNewUIHandler(t *testing.T) {
+	handler := NewUIHandler()
+	assert.Implements(t, http.Handler(nil), handler)
+}
+
+func TestNewJSHandler(t *testing.T) {
+	handler := NewJSHandler()
+	assert.Implements(t, http.Handler(nil), handler)
+}
