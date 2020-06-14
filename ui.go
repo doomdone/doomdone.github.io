086fbc728d2638d453f558a396e4b75a1a5c7d84
domino <damino@gmail.com>
2020-06-14 13:26:36 +0300

    added tests

--- ui.go
+++ ui.go
@@ -1,6 +1,10 @@
 package ui
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+	"os"
+)
 
 const (
 	jsBundlePath = "./static/js/bundle.js"
@@ -8,12 +12,22 @@ const (
 )
 
 func NewUIHandler() http.HandlerFunc {
+	file, err := os.Open(htmlFilePath)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(file.Name())
 	return func(rw http.ResponseWriter, r *http.Request) {
 		http.ServeFile(rw, r, htmlFilePath)
 	}
 }
 
 func NewJSHandler() http.HandlerFunc {
+	file, err := os.Open(htmlFilePath)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(file.Name())
 	return func(rw http.ResponseWriter, r *http.Request) {
 		http.ServeFile(rw, r, jsBundlePath)
 	}
--- ui_test.go
+++ ui_test.go
@@ -3,16 +3,22 @@ package ui
 import (
 	"github.com/stretchr/testify/assert"
 	"net/http"
+	"net/http/httptest"
 	"testing"
 )
 
 func TestNewUIHandler(t *testing.T) {
 	handler := NewUIHandler()
-	assert.Implements(t, http.HandlerFunc(nil), handler)
+	rw := httptest.NewRecorder()
+	r := httptest.NewRequest(http.MethodGet, "/", nil)
+	handler(rw, r)
+	assert.Equal(t, http.StatusOK, rw.Code)
 }
 
 func TestNewJSHandler(t *testing.T) {
 	handler := NewJSHandler()
-	assert.Implements(t, http.HandlerFunc(nil), handler)
-	//http.Handle("/js", handler)
+	rw := httptest.NewRecorder()
+	r := httptest.NewRequest(http.MethodGet, "/js", nil)
+	handler(rw, r)
+	assert.Equal(t, http.StatusOK, rw.Code)
 }
