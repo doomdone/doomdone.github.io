fc6362616359337447c68a6b9bca93653dbfc57f
domino <damino@gmail.com>
2020-07-04 14:26:43 +0300

    testing filepaths №3

--- ui.go
+++ ui.go
@@ -3,20 +3,28 @@ package ui
 import (
 	"fmt"
 	"net/http"
+	"os"
 	"path/filepath"
 )
 
 const (
-	jsBundlePath = "static/js/bundle.js"
-	htmlFilePath = "static/start.html"
+	jsBundlePath = "./static/js/bundle.js"
+	htmlFilePath = "./static/start.html"
 )
 
 func NewUIHandler() http.HandlerFunc {
-	filePath, err := filepath.Abs(htmlFilePath)
+	absolute, err := filepath.Abs(htmlFilePath)
 	if err != nil {
 		panic(err)
 	}
-	fmt.Println(filePath)
+	fmt.Println(absolute)
+
+	file, err := os.Open(absolute)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(file.Name())
+
 	return func(rw http.ResponseWriter, r *http.Request) {
 		http.ServeFile(rw, r, htmlFilePath)
 	}
@@ -28,6 +36,13 @@ func NewJSHandler() http.HandlerFunc {
 		panic(err)
 	}
 	fmt.Println(filePath)
+
+	file, err := os.Open(htmlFilePath)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(file.Name())
+
 	return func(rw http.ResponseWriter, r *http.Request) {
 		http.ServeFile(rw, r, jsBundlePath)
 	}
