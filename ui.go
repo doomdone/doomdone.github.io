e4b6061590d5f1cd2cd05b3235e1d3d623442182
domino <damino@gmail.com>
2020-07-05 21:54:38 +0300

    correcting static paths №6

--- ui.go
+++ ui.go
@@ -1,10 +1,7 @@
 package ui
 
 import (
-	"fmt"
 	"net/http"
-	"os"
-	"path/filepath"
 )
 
 const (
@@ -13,18 +10,6 @@ const (
 )
 
 func NewUIHandler() http.HandlerFunc {
-	absolute, err := filepath.Abs(htmlFilePath)
-	if err != nil {
-		panic(err)
-	}
-	fmt.Println(absolute)
-
-	file, err := os.Open(htmlFilePath)
-	if err != nil {
-		panic(err)
-	}
-	fmt.Println(file.Name())
-
 	return func(rw http.ResponseWriter, r *http.Request) {
 		http.ServeFile(rw, r, htmlFilePath)
 	}
@@ -32,18 +17,6 @@ func NewUIHandler() http.HandlerFunc {
 }
 
 func NewJSHandler() http.HandlerFunc {
-	filePath, err := filepath.Abs(jsBundlePath)
-	if err != nil {
-		panic(err)
-	}
-	fmt.Println(filePath)
-
-	file, err := os.Open(htmlFilePath)
-	if err != nil {
-		panic(err)
-	}
-	fmt.Println(file.Name())
-
 	return func(rw http.ResponseWriter, r *http.Request) {
 		http.ServeFile(rw, r, jsBundlePath)
 	}
