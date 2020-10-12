f05eb672509ffc7952e3c978be5bf6140d9d5a56
domino <damino@gmail.com>
2020-10-12 19:14:12 +0300

    correcting static paths №9

--- ui.go
+++ ui.go
@@ -2,10 +2,12 @@ package ui
 
 import (
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
 	"path/filepath"
+	"reflect"
 )
 
 const (
@@ -13,24 +15,44 @@ const (
 	htmlFilePath = "./static/start.html"
 )
 
+var jsBundle, htmlFile []byte
+
+func init() {
+	jsBundle, err := ioutil.ReadFile(jsBundlePath)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	htmlFile, err := ioutil.ReadFile(htmlFilePath)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+}
+
 func NewUIHandler() http.HandlerFunc {
-	absolute, err := filepath.Abs(htmlFilePath)
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Printf("filepath.Abs(htmlFilePath) : %s\n", absolute)
-
-	twd, err := os.Getwd()
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Printf("os.Getwd() : %s\n", twd)
-
-	file, err := os.Open(htmlFilePath)
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Printf("os.Open(htmlFilePath) : %s\n", file.Name())
+	fmt.Println(string(jsBundle))
+	//absolute, err := filepath.Abs(htmlFilePath)
+	//if err != nil {
+	//	log.Fatal(err)
+	//}
+	//fmt.Printf("filepath.Abs(htmlFilePath) : %s\n", absolute)
+	//
+	//twd, err := os.Getwd()
+	//if err != nil {
+	//	log.Fatal(err)
+	//}
+	//fmt.Printf("os.Getwd() : %s\n", twd)
+	//
+	//file, err := os.Open(htmlFilePath)
+	//if err != nil {
+	//	log.Fatal(err)
+	//}
+	//fmt.Printf("os.Open(htmlFilePath) : %s\n", file.Name())
+	//
+	//var reflectUI ui
+	//pkgPath := reflect.TypeOf(reflectUI).PkgPath()
+	//fmt.Printf("PkgPath():%s\n", pkgPath)
 
 	return func(rw http.ResponseWriter, r *http.Request) {
 		http.ServeFile(rw, r, htmlFilePath)
@@ -39,19 +61,23 @@ func NewUIHandler() http.HandlerFunc {
 }
 
 func NewJSHandler() http.HandlerFunc {
-	filePath, err := filepath.Abs(jsBundlePath)
-	if err != nil {
-		panic(err)
-	}
-	fmt.Println(filePath)
-
-	file, err := os.Open(htmlFilePath)
-	if err != nil {
-		panic(err)
-	}
-	fmt.Println(file.Name())
+	fmt.Println(string(jsBundle))
+	//filePath, err := filepath.Abs(jsBundlePath)
+	//if err != nil {
+	//	panic(err)
+	//}
+	//fmt.Println(filePath)
+	//
+	//file, err := os.Open(htmlFilePath)
+	//if err != nil {
+	//	panic(err)
+	//}
+	//fmt.Println(file.Name())
 
 	return func(rw http.ResponseWriter, r *http.Request) {
 		http.ServeFile(rw, r, jsBundlePath)
 	}
+}
+
+type ui struct {
 }
