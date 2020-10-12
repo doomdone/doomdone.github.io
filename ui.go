cb0ab47577c9e62aa3985a924e6a715f9360deac
domino <damino@gmail.com>
2020-10-12 17:49:14 +0300

    correcting static paths №7

--- ui.go
+++ ui.go
@@ -1,15 +1,37 @@
 package ui
 
 import (
+	"fmt"
+	"log"
 	"net/http"
+	"os"
+	"path/filepath"
 )
 
 const (
-	jsBundlePath = "github.com/damino/ui/static/js/bundle.js"
-	htmlFilePath = "github.com/damino/ui/static/start.html"
+	jsBundlePath = "./static/js/bundle.js"
+	htmlFilePath = "./static/start.html"
 )
 
 func NewUIHandler() http.HandlerFunc {
+	absolute, err := filepath.Abs(htmlFilePath)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(absolute)
+
+	twd, err := os.Getwd()
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(twd)
+
+	file, err := os.Open(htmlFilePath)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(file.Name())
+
 	return func(rw http.ResponseWriter, r *http.Request) {
 		http.ServeFile(rw, r, htmlFilePath)
 	}
@@ -17,6 +39,18 @@ func NewUIHandler() http.HandlerFunc {
 }
 
 func NewJSHandler() http.HandlerFunc {
+	filePath, err := filepath.Abs(jsBundlePath)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(filePath)
+
+	file, err := os.Open(htmlFilePath)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(file.Name())
+
 	return func(rw http.ResponseWriter, r *http.Request) {
 		http.ServeFile(rw, r, jsBundlePath)
 	}
